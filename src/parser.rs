0b344a86445aa0b82cc7ff39a75aa1d139a0f913
Ivan Bodrov <ibodrov@gmail.com>
2023-10-13 18:54:44 -0400

    initial commit

---
+++ src/parser.rs
@@ -0,0 +1,476 @@
+use std::{collections::HashMap, fmt::Display};
+
+use crate::model::{ConcordDocument, ConcordFlow, ConcordFlowStep, Value};
+
+pub type Event = yaml_rust2::Event;
+pub type Marker = yaml_rust2::scanner::Marker;
+
+pub struct Input {
+    items: Vec<(Event, Marker)>,
+    idx: usize,
+}
+
+impl TryFrom<&str> for Input {
+    type Error = ParseError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut parser = yaml_rust2::parser::Parser::new(value.chars());
+        let mut items = Vec::new();
+        loop {
+            let (ev, marker) = parser.next_token()?;
+            let done = ev == Event::StreamEnd;
+            items.push((ev, marker));
+            if done {
+                break;
+            }
+        }
+        Ok(Input { items, idx: 0 })
+    }
+}
+
+macro_rules! match_next {
+    ($input:ident, $pat:pat) => {
+        match $input.next()? {
+            (ev @ $pat, marker) => Ok((ev, marker)),
+            (ev, marker) => Err(ParseError {
+                marker: Some(marker.clone()),
+                kind: ErrorKind::UnexpectedSyntax,
+                msg: format!("Expected {}, got {ev:?}", stringify!($pat)),
+            }),
+        }
+    };
+}
+
+macro_rules! parse_until {
+    ($input:ident, $pat:pat, $parser:ident) => {{
+        let mut items = Vec::new();
+        loop {
+            let item = $parser($input)?;
+            items.push(item);
+            if matches!($input.peek()?, ($pat, _)) {
+                break;
+            }
+        }
+        items
+    }};
+}
+
+impl Input {
+    fn check_eof(&self) -> Result<(), ParseError> {
+        if self.idx >= self.items.len() {
+            Err(ParseError {
+                marker: None,
+                kind: ErrorKind::ScanError,
+                msg: "EOF".to_string(),
+            })
+        } else {
+            Ok(())
+        }
+    }
+
+    fn next(&mut self) -> Result<(Event, Marker), ParseError> {
+        self.check_eof()?;
+        let (event, marker) = &self.items[self.idx];
+        println!("next: [{}] {event:?} @ {marker:?}", self.idx);
+        self.idx += 1;
+        Ok((event.clone(), *marker))
+    }
+
+    fn next_stream_start(&mut self) -> Result<(Event, Marker), ParseError> {
+        match_next!(self, Event::StreamStart)
+    }
+
+    fn next_stream_end(&mut self) -> Result<(Event, Marker), ParseError> {
+        match_next!(self, Event::StreamEnd)
+    }
+
+    fn next_document_start(&mut self) -> Result<(Event, Marker), ParseError> {
+        match_next!(self, Event::DocumentStart)
+    }
+
+    fn next_document_end(&mut self) -> Result<(Event, Marker), ParseError> {
+        match_next!(self, Event::DocumentEnd)
+    }
+
+    fn next_mapping_start(&mut self) -> Result<(Event, Marker), ParseError> {
+        match_next!(self, Event::MappingStart(..))
+    }
+
+    fn next_mapping_end(&mut self) -> Result<(Event, Marker), ParseError> {
+        match_next!(self, Event::MappingEnd)
+    }
+
+    fn next_sequence_start(&mut self) -> Result<(Event, Marker), ParseError> {
+        match_next!(self, Event::SequenceStart(..))
+    }
+
+    fn next_sequence_end(&mut self) -> Result<(Event, Marker), ParseError> {
+        match_next!(self, Event::SequenceEnd)
+    }
+
+    fn next_string(&mut self) -> Result<(String, Marker), ParseError> {
+        match self.next()? {
+            (Event::Scalar(value, ..), marker) => Ok((value.to_owned(), marker)),
+            (ev, marker) => Err(ParseError {
+                marker: Some(marker),
+                kind: ErrorKind::UnexpectedSyntax,
+                msg: format!("Expected to peek a scalar, got {ev:?}"),
+            }),
+        }
+    }
+
+    fn next_string_constant(&mut self, value: &str) -> Result<(), ParseError> {
+        match self.next()? {
+            (Event::Scalar(scalar, ..), _) if scalar == value => Ok(()),
+            (ev, marker) => Err(ParseError {
+                marker: Some(marker),
+                kind: ErrorKind::UnexpectedSyntax,
+                msg: format!("Expected a string {value}, got {ev:?}"),
+            }),
+        }
+    }
+
+    fn next_kv(&mut self) -> Result<(String, Value), ParseError> {
+        let (key, _) = self.next_string()?;
+        let (value, _) = self.next_value()?;
+        Ok((key, value))
+    }
+
+    fn next_value(&mut self) -> Result<(Value, Marker), ParseError> {
+        match self.next()? {
+            (Event::Scalar(scalar, style, ..), marker) => {
+                use yaml_rust2::scanner::TScalarStyle::*;
+                match style {
+                    SingleQuoted | DoubleQuoted => Ok((Value::String(scalar), marker)),
+                    Plain => {
+                        if parse_f64(&scalar).is_ok() {
+                            Ok((Value::Float(scalar), marker))
+                        } else if let Ok(value) = scalar.parse::<i64>() {
+                            Ok((Value::Integer(value), marker))
+                        } else if let Ok(value) = scalar.parse::<bool>() {
+                            // TODO handle "yes/no", etc
+                            Ok((Value::Boolean(value), marker))
+                        } else {
+                            Ok((Value::String(scalar), marker))
+                        }
+                    }
+                    _ => Err(ParseError {
+                        marker: Some(marker),
+                        kind: ErrorKind::UnexpectedSyntax,
+                        msg: format!("Unsupported value syntax, got \"{scalar}\" as {style:?}"),
+                    }),
+                }
+            }
+            (Event::MappingStart(..), marker) => {
+                let result = parse_until!(self, Event::MappingEnd, next_kv)
+                    .into_iter()
+                    .collect();
+                self.next_mapping_end()?;
+                Ok((Value::Mapping(result), marker))
+            }
+            (ev, marker) => Err(ParseError {
+                marker: Some(marker),
+                kind: ErrorKind::UnexpectedSyntax,
+                msg: format!("Expected a value, got {ev:?}"),
+            }),
+        }
+    }
+
+    fn peek(&mut self) -> Result<&(Event, Marker), ParseError> {
+        self.check_eof()?;
+        Ok(&self.items[self.idx])
+    }
+
+    fn peek_string(&mut self) -> Result<Option<(String, Marker)>, ParseError> {
+        match self.peek()? {
+            (Event::Scalar(value, ..), marker) => Ok(Some((value.to_owned(), *marker))),
+            (ev, marker) => Err(ParseError {
+                marker: Some(*marker),
+                kind: ErrorKind::UnexpectedSyntax,
+                msg: format!("Expected to peek a scalar, got {ev:?}"),
+            }),
+        }
+    }
+
+    fn peek_string_constant(&mut self, value: &str) -> Result<bool, ParseError> {
+        match self.peek()? {
+            (Event::Scalar(scalar, ..), _) => Ok(scalar == value),
+            _ => Ok(false),
+        }
+    }
+}
+
+// just a helper to simplify parse_until! usage
+fn next_kv(input: &mut Input) -> Result<(String, Value), ParseError> {
+    input.next_kv()
+}
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    ScanError,
+    UnexpectedSyntax,
+}
+
+#[derive(Debug)]
+pub struct ParseError {
+    marker: Option<Marker>,
+    kind: ErrorKind,
+    msg: String,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} @ {:?}: {}", self.kind, self.marker, self.msg)
+    }
+}
+
+impl From<yaml_rust2::ScanError> for ParseError {
+    fn from(value: yaml_rust2::ScanError) -> Self {
+        Self {
+            marker: Some(*value.marker()),
+            kind: ErrorKind::ScanError,
+            msg: value.to_string(),
+        }
+    }
+}
+
+// from https://github.com/chyh1990/yaml-rust/blob/master/src/yaml.rs
+// with minor changes (Option -> Result)
+fn parse_f64(value: &str) -> Result<f64, ParseError> {
+    match value {
+        ".inf" | ".Inf" | ".INF" | "+.inf" | "+.Inf" | "+.INF" => Ok(f64::INFINITY),
+        "-.inf" | "-.Inf" | "-.INF" => Ok(f64::NEG_INFINITY),
+        ".nan" | "NaN" | ".NAN" => Ok(f64::NAN),
+        _ => value.parse::<f64>().map_err(|e| ParseError {
+            marker: None,
+            kind: ErrorKind::UnexpectedSyntax,
+            msg: format!("Invalid float number {value}: {e}"),
+        }),
+    }
+}
+
+fn parse_task_call(input: &mut Input) -> Result<ConcordFlowStep, ParseError> {
+    let (name, _) = input.next_string()?;
+    let mut input_parameters = HashMap::new();
+    if input.peek_string_constant("in")? {
+        input.next()?;
+        input.next_mapping_start()?;
+        let kvs = parse_until!(input, Event::MappingEnd, next_kv);
+        input_parameters.extend(kvs);
+        input.next_mapping_end()?;
+    };
+    Ok(ConcordFlowStep::TaskCall {
+        name,
+        input: input_parameters,
+    })
+}
+
+fn parse_step(input: &mut Input) -> Result<ConcordFlowStep, ParseError> {
+    input.next_mapping_start()?;
+
+    let step = match input.next()? {
+        (Event::Scalar(key, ..), _) if key == "log" => {
+            let (msg, _) = input.next_string()?;
+            ConcordFlowStep::TaskCall {
+                name: "log".to_owned(),
+                input: HashMap::from([("msg".to_owned(), Value::String(msg))]),
+            }
+        }
+        (Event::Scalar(key, ..), _) if key == "task" => parse_task_call(input)?,
+        (ev, marker) => {
+            return Err(ParseError {
+                marker: Some(marker),
+                kind: ErrorKind::UnexpectedSyntax,
+                msg: format!("Expected a flow step, got {ev:?}"),
+            })
+        }
+    };
+
+    input.next_mapping_end()?;
+
+    Ok(step)
+}
+
+fn parse_flow(input: &mut Input) -> Result<ConcordFlow, ParseError> {
+    let (name, _) = input.next_string()?;
+    input.next_sequence_start()?;
+    let steps = parse_until!(input, Event::SequenceEnd, parse_step);
+    input.next_sequence_end()?;
+    Ok(ConcordFlow { name, steps })
+}
+
+fn parse_flows(input: &mut Input) -> Result<Vec<ConcordFlow>, ParseError> {
+    input.next_string_constant("flows")?;
+    input.next_mapping_start()?;
+    let result = parse_until!(input, Event::MappingEnd, parse_flow);
+    input.next_mapping_end()?;
+    Ok(result)
+}
+
+fn parse_document(input: &mut Input) -> Result<ConcordDocument, ParseError> {
+    input.next_document_start()?;
+    input.next_mapping_start()?;
+
+    // top-level elements
+    let mut flows = Vec::new();
+    if let Some((top_level_element, marker)) = input.peek_string()? {
+        match top_level_element.as_str() {
+            "flows" => {
+                for flow in parse_flows(input)? {
+                    flows.push(flow);
+                }
+            }
+            element => {
+                return Err(ParseError {
+                    marker: Some(marker),
+                    kind: ErrorKind::UnexpectedSyntax,
+                    msg: format!("Unexpected top-level element {element}"),
+                })
+            }
+        }
+    }
+
+    input.next_mapping_end()?;
+    input.next_document_end()?;
+
+    Ok(ConcordDocument { flows })
+}
+
+pub fn parse_stream(input: &mut Input) -> Result<Vec<ConcordDocument>, ParseError> {
+    input.next_stream_start()?;
+    let result = parse_until!(input, Event::StreamEnd, parse_document);
+    input.next_stream_end()?;
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn matches_float(value: Option<&Value>, expected: &str) -> bool {
+        matches!(value, Some(Value::Float(value)) if value == expected)
+    }
+
+    #[test]
+    fn hello_world() {
+        let src = r#"
+        flows:
+          default:
+            - log: "Hello!"
+        "#;
+
+        let mut input = Input::try_from(src).unwrap();
+        let result = parse_stream(&mut input).unwrap();
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].flows.len(), 1);
+        assert_eq!(result[0].flows[0].name, "default");
+        assert_eq!(result[0].flows[0].steps.len(), 1);
+    }
+
+    #[test]
+    fn multiple_flows() {
+        let src = r#"
+        flows:
+          default:
+            - log: "Hello!"
+          another_one:
+            - log: "Yo!"
+        "#;
+
+        let mut input = Input::try_from(src).unwrap();
+        let result = parse_stream(&mut input).unwrap();
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].flows.len(), 2);
+        assert_eq!(result[0].flows[0].name, "default");
+        assert_eq!(result[0].flows[0].steps.len(), 1);
+        assert_eq!(result[0].flows[1].name, "another_one");
+        assert_eq!(result[0].flows[1].steps.len(), 1);
+    }
+
+    #[test]
+    fn multiple_docs() {
+        let src = "---\nflows:\n  default:\n    - log: \"Hello!\"\n---\nflows:\n  another_one:\n    - log: \"Bye!\"";
+
+        let mut input = Input::try_from(src).unwrap();
+        let result = parse_stream(&mut input).unwrap();
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0].flows.len(), 1);
+        assert_eq!(result[0].flows[0].name, "default");
+        assert_eq!(result[0].flows[0].steps.len(), 1);
+        assert_eq!(result[1].flows.len(), 1);
+        assert_eq!(result[1].flows[0].name, "another_one");
+        assert_eq!(result[1].flows[0].steps.len(), 1);
+    }
+
+    #[test]
+    fn multiple_steps() {
+        let src = r#"
+        flows:
+          default:
+            - log: "Hello!"
+            - log: "Bye!"
+        "#;
+
+        let mut input = Input::try_from(src).unwrap();
+        let result = parse_stream(&mut input).unwrap();
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].flows.len(), 1);
+        assert_eq!(result[0].flows[0].name, "default");
+        assert_eq!(result[0].flows[0].steps.len(), 2);
+    }
+
+    #[test]
+    fn invalid_top_level_element() {
+        let src = r#"
+        flows:
+          default:
+            - log: "Hello!"
+        
+        gizmos: ["a", 1, false]
+        "#;
+
+        let mut input = Input::try_from(src).unwrap();
+        let result = parse_stream(&mut input);
+        assert!(matches!(
+            result,
+            Err(ParseError {
+                marker: Some(..),
+                ..
+            })
+        ));
+    }
+
+    #[test]
+    fn task_call() {
+        let src = r#"
+        flows:
+          default:
+            - task: foo
+              in:
+                a: 1.23456789
+                b: "Hello!"
+                c: false
+                d:
+                  x:
+                    y:
+                      z: true
+        "#;
+
+        let mut input = Input::try_from(src).unwrap();
+        let result = parse_stream(&mut input).unwrap();
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].flows.len(), 1);
+        assert_eq!(result[0].flows[0].steps.len(), 1);
+        assert!(match &result[0].flows[0].steps[0] {
+            ConcordFlowStep::TaskCall { name, input } => {
+                assert_eq!(name, "foo");
+                assert_eq!(input.len(), 4);
+                assert!(matches_float(input.get("a"), "1.23456789"));
+                assert!(matches!(input.get("b"), Some(Value::String(value)) if value == "Hello!"));
+                assert!(matches!(input.get("c"), Some(Value::Boolean(false))));
+                assert!(matches!(input.get("d"), Some(Value::Mapping(..))));
+                true
+            }
+        });
+    }
+}
---
+++ src/vm.rs
@@ -0,0 +1,185 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct VmError {
+    msg: String,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+pub struct ThreadId(u32);
+
+pub enum StateCommand {
+    PopCommand(ThreadId),
+    PopFrame(ThreadId),
+    PopThread,
+}
+
+/// A unit of execution. Can return StateCommands to modify the VM state.
+pub trait Command {
+    fn eval(&self, thread_id: ThreadId) -> Result<Option<StateCommand>, VmError>;
+}
+
+#[derive(Default)]
+pub struct Frame {
+    /// Stack of commands. The last item in the vector is the current command.
+    commands: Vec<Box<dyn Command>>,
+}
+
+pub struct Thread {
+    id: ThreadId,
+    /// Frames. Each frame has it's own stack of commands.
+    frames: Vec<Frame>,
+}
+
+#[derive(Default)]
+pub struct VM {
+    /// Virtual threads. Each thread has its own stack of frames.
+    threads: Vec<Thread>,
+}
+
+impl Thread {
+    fn eval(&mut self) -> Result<Option<StateCommand>, VmError> {
+        let thread_id = self.id;
+
+        if let Some(frame) = self.frames.last_mut() {
+            frame.eval(thread_id)
+        } else {
+            // no more frames
+            Ok(Some(StateCommand::PopThread))
+        }
+    }
+}
+
+impl Frame {
+    fn eval(&mut self, thread_id: ThreadId) -> Result<Option<StateCommand>, VmError> {
+        if let Some(command) = self.commands.last_mut() {
+            command.eval(thread_id)
+        } else {
+            // no more commands
+            Ok(Some(StateCommand::PopFrame(thread_id)))
+        }
+    }
+}
+
+impl VM {
+    pub fn new() -> Self {
+        let frames = vec![Frame {
+            commands: vec![
+                Box::new(commands::TaskCallCommand {
+                    task_name: "second".to_owned(),
+                }),
+                Box::new(commands::TaskCallCommand {
+                    task_name: "first".to_owned(),
+                }),
+            ],
+        }];
+
+        VM {
+            threads: vec![Thread {
+                id: ThreadId(0),
+                frames,
+            }],
+        }
+    }
+
+    pub fn run(&mut self, thread_id: ThreadId) -> Result<(), VmError> {
+        loop {
+            let thread = self.get_thread_mut(thread_id).ok_or_else(|| VmError {
+                msg: format!("Thread {thread_id:?} not found"),
+            })?;
+
+            if let Some(command) = thread.eval()? {
+                match command {
+                    StateCommand::PopCommand(thread_id) => {
+                        self.pop_command(thread_id)?;
+                    }
+                    StateCommand::PopFrame(thread_id) => {
+                        self.pop_frame(thread_id)?;
+                    }
+                    StateCommand::PopThread => {
+                        self.pop_thread(thread_id)?;
+                        break;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn get_thread_mut(&mut self, thread_id: ThreadId) -> Option<&mut Thread> {
+        self.threads
+            .iter_mut()
+            .find(|thread| thread.id == thread_id)
+    }
+
+    fn assert_thread_mut(&mut self, thread_id: ThreadId) -> Result<&mut Thread, VmError> {
+        self.get_thread_mut(thread_id).ok_or_else(|| VmError {
+            msg: format!("Thread {thread_id:?} does not exist"),
+        })
+    }
+
+    fn assert_current_frame_mut(&mut self, thread_id: ThreadId) -> Result<&mut Frame, VmError> {
+        let thread = self.assert_thread_mut(thread_id)?;
+        thread.frames.last_mut().ok_or_else(|| VmError {
+            msg: format!("Thread {thread_id:?} has no current frame"),
+        })
+    }
+
+    fn pop_frame(&mut self, thread_id: ThreadId) -> Result<(), VmError> {
+        let thread = self.assert_thread_mut(thread_id)?;
+        if thread.frames.is_empty() {
+            return Err(VmError {
+                msg: "No more frames to pop".to_owned(),
+            });
+        }
+        thread.frames.pop();
+        Ok(())
+    }
+
+    fn pop_command(&mut self, thread_id: ThreadId) -> Result<(), VmError> {
+        let frame = self.assert_current_frame_mut(thread_id)?;
+        if frame.commands.is_empty() {
+            return Err(VmError {
+                msg: "No more commands to pop".to_owned(),
+            });
+        }
+        frame.commands.pop();
+        Ok(())
+    }
+
+    fn pop_thread(&mut self, thread_id: ThreadId) -> Result<(), VmError> {
+        let idx = self
+            .threads
+            .binary_search_by_key(&thread_id, |thread| thread.id)
+            .map_err(|_| VmError {
+                msg: format!("Can't remove non-existent thread {thread_id:?}"),
+            })?;
+        self.threads.remove(idx);
+        Ok(())
+    }
+}
+
+mod commands {
+    use super::{Command, StateCommand, ThreadId, VmError};
+
+    pub struct TaskCallCommand {
+        pub task_name: String,
+    }
+
+    impl Command for TaskCallCommand {
+        fn eval(&self, thread_id: ThreadId) -> Result<Option<StateCommand>, VmError> {
+            println!("[{:?}] {} call!", thread_id, self.task_name);
+            Ok(Some(StateCommand::PopCommand(thread_id)))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hello_world() {
+        VM::new().run(ThreadId(0)).unwrap();
+    }
+}
