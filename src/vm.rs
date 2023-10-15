bedc776b7711134fc2da9904a0e6693fa3bf2a1e
Ivan Bodrov <ibodrov@gmail.com>
2023-10-14 20:16:51 -0400

    wip

--- src/vm.rs
+++ src/vm.rs
@@ -8,100 +8,67 @@ pub struct VmError {
 pub struct ThreadId(u32);
 
 pub enum StateCommand {
-    Continue, // TODO use Option<StateCommand> instead?
-    PopCommand,
-    PopFrame,
+    PopCommand(ThreadId),
+    PopFrame(ThreadId),
     DeleteThread(ThreadId),
 }
 
+/// A unit of execution. Can return StateCommands to modify the VM state.
 pub trait Command {
-    fn eval(&self, thread_id: ThreadId) -> Result<StateCommand, VmError>;
+    fn eval(&self, thread_id: ThreadId) -> Result<Option<StateCommand>, VmError>;
 }
 
 #[derive(Default)]
 pub struct Frame {
+    /// Stack of commands. The last item in the vector is the current command.
     commands: Vec<Box<dyn Command>>,
-}
-
-#[derive(Default)]
-pub struct VM {
-    threads: Vec<Thread>,
 }
 
 pub struct Thread {
     id: ThreadId,
+    /// Frames. Each frame has it's own stack of commands.
     frames: Vec<Frame>,
 }
 
+#[derive(Default)]
+pub struct VM {
+    /// Virtual threads. Each thread has its own stack of frames.
+    threads: Vec<Thread>,
+}
+
 impl Thread {
-    fn get_current_frame_mut(&mut self) -> Option<&mut Frame> {
-        if self.frames.is_empty() {
-            return None;
-        }
-        let idx = self.frames.len() - 1;
-        self.frames.get_mut(idx)
-    }
-
-    fn pop_frame(&mut self) -> Result<(), VmError> {
-        if self.frames.is_empty() {
-            return Err(VmError {
-                msg: "No more frames to pop".to_owned(),
-            });
-        }
-        self.frames.pop();
-        Ok(())
-    }
-
-    fn eval(&mut self) -> Result<StateCommand, VmError> {
+    fn eval(&mut self) -> Result<Option<StateCommand>, VmError> {
         let thread_id = self.id;
 
-        if let Some(frame) = self.get_current_frame_mut() {
-            if let Some(command) = frame.get_current_command() {
-                match command.eval(thread_id)? {
-                    StateCommand::Continue => Ok(StateCommand::Continue),
-                    StateCommand::PopCommand => {
-                        frame.pop_command()?;
-                        Ok(StateCommand::Continue)
-                    }
-                    StateCommand::PopFrame => {
-                        self.pop_frame()?;
-                        Ok(StateCommand::Continue)
-                    }
-                    cmd @ StateCommand::DeleteThread(..) => Ok(cmd),
-                }
-            } else {
-                Ok(StateCommand::DeleteThread(self.id))
-            }
+        if let Some(frame) = self.frames.last_mut() {
+            frame.eval(thread_id)
         } else {
-            Ok(StateCommand::DeleteThread(self.id))
+            Ok(Some(StateCommand::DeleteThread(self.id)))
         }
     }
 }
 
 impl Frame {
-    fn get_current_command(&mut self) -> Option<&dyn Command> {
-        if self.commands.is_empty() {
-            return None;
+    fn eval(&mut self, thread_id: ThreadId) -> Result<Option<StateCommand>, VmError> {
+        if let Some(command) = self.commands.last_mut() {
+            command.eval(thread_id)
+        } else {
+            Ok(Some(StateCommand::DeleteThread(thread_id)))
         }
-        let idx = self.commands.len() - 1;
-        self.commands.get(idx).map(|cmd| &**cmd)
-    }
-
-    fn pop_command(&mut self) -> Result<(), VmError> {
-        if self.commands.is_empty() {
-            return Err(VmError {
-                msg: "No more commands to pop".to_owned(),
-            });
-        }
-        self.commands.pop();
-        Ok(())
     }
 }
 
 impl VM {
     pub fn new() -> Self {
         let frames = vec![Frame {
-            commands: vec![Box::new(commands::TaskCallCommand {})],
+            commands: vec![
+                Box::new(commands::TaskCallCommand {
+                    task_name: "second".to_owned(),
+                }),
+                Box::new(commands::TaskCallCommand {
+                    task_name: "first".to_owned(),
+                }),
+            ],
         }];
 
         VM {
@@ -112,16 +79,51 @@ impl VM {
         }
     }
 
-    fn get_thread_mut(&mut self, thread_id: &ThreadId) -> Option<&mut Thread> {
+    fn get_thread_mut(&mut self, thread_id: ThreadId) -> Option<&mut Thread> {
         self.threads
             .iter_mut()
-            .find(|thread| thread.id == *thread_id)
+            .find(|thread| thread.id == thread_id)
     }
 
-    fn delete_thread(&mut self, thread_id: &ThreadId) -> Result<(), VmError> {
+    fn assert_thread_mut(&mut self, thread_id: ThreadId) -> Result<&mut Thread, VmError> {
+        self.get_thread_mut(thread_id).ok_or_else(|| VmError {
+            msg: format!("Thread {thread_id:?} does not exist"),
+        })
+    }
+
+    fn assert_current_frame_mut(&mut self, thread_id: ThreadId) -> Result<&mut Frame, VmError> {
+        let thread = self.assert_thread_mut(thread_id)?;
+        thread.frames.last_mut().ok_or_else(|| VmError {
+            msg: format!("Thread {thread_id:?} has no current frame"),
+        })
+    }
+
+    fn pop_frame(&mut self, thread_id: ThreadId) -> Result<(), VmError> {
+        let thread = self.assert_thread_mut(thread_id)?;
+        if thread.frames.is_empty() {
+            return Err(VmError {
+                msg: "No more frames to pop".to_owned(),
+            });
+        }
+        thread.frames.pop();
+        Ok(())
+    }
+
+    fn pop_command(&mut self, thread_id: ThreadId) -> Result<(), VmError> {
+        let frame = self.assert_current_frame_mut(thread_id)?;
+        if frame.commands.is_empty() {
+            return Err(VmError {
+                msg: "No more commands to pop".to_owned(),
+            });
+        }
+        frame.commands.pop();
+        Ok(())
+    }
+
+    fn delete_thread(&mut self, thread_id: ThreadId) -> Result<(), VmError> {
         let idx = self
             .threads
-            .binary_search_by_key(&thread_id, |thread| &thread.id)
+            .binary_search_by_key(&thread_id, |thread| thread.id)
             .map_err(|_| VmError {
                 msg: format!("Can't remove non-existent thread {thread_id:?}"),
             })?;
@@ -129,7 +131,7 @@ impl VM {
         Ok(())
     }
 
-    pub fn run(&mut self, thread_id: &ThreadId) -> Result<(), VmError> {
+    pub fn run(&mut self, thread_id: ThreadId) -> Result<(), VmError> {
         loop {
             if self.threads.is_empty() {
                 break;
@@ -137,7 +139,7 @@ impl VM {
 
             let state_command;
             if let Some(thread) = self.get_thread_mut(thread_id) {
-                state_command = Some(thread.eval()?);
+                state_command = thread.eval()?;
                 if thread.frames.is_empty() {
                     self.delete_thread(thread_id)?;
                 }
@@ -145,12 +147,17 @@ impl VM {
                 break;
             }
 
-            match state_command.unwrap_or(StateCommand::Continue) {
-                StateCommand::Continue => {}
-                StateCommand::PopCommand => todo!(),
-                StateCommand::PopFrame => todo!(),
-                StateCommand::DeleteThread(thread_id) => {
-                    self.delete_thread(&thread_id)?;
+            if let Some(command) = state_command {
+                match command {
+                    StateCommand::PopCommand(thread_id) => {
+                        self.pop_command(thread_id)?;
+                    }
+                    StateCommand::PopFrame(thread_id) => {
+                        self.pop_frame(thread_id)?;
+                    }
+                    StateCommand::DeleteThread(thread_id) => {
+                        self.delete_thread(thread_id)?;
+                    }
                 }
             }
         }
@@ -161,12 +168,14 @@ impl VM {
 mod commands {
     use super::{Command, StateCommand, ThreadId, VmError};
 
-    pub struct TaskCallCommand {}
+    pub struct TaskCallCommand {
+        pub task_name: String,
+    }
 
     impl Command for TaskCallCommand {
-        fn eval(&self, thread_id: ThreadId) -> Result<StateCommand, VmError> {
-            println!("[{:?}] Task call!", thread_id);
-            Ok(StateCommand::PopCommand)
+        fn eval(&self, thread_id: ThreadId) -> Result<Option<StateCommand>, VmError> {
+            println!("[{:?}] {} call!", thread_id, self.task_name);
+            Ok(Some(StateCommand::PopCommand(thread_id)))
         }
     }
 }
@@ -177,6 +186,6 @@ mod tests {
 
     #[test]
     fn hello_world() {
-        VM::new().run(&ThreadId(0)).unwrap();
+        VM::new().run(ThreadId(0)).unwrap();
     }
 }
